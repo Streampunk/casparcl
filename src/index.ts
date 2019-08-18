aa494e47318b8219fa11f2de7b9432901eac1c90
Richard Cartwright <spark@streampunk.media>
2019-08-18 14:37:31 +0200

    chore: highland simple mix with promises

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 	import H from 'highland'
 
-let counter = 0
+let [counter1, counter2] = [0, 0]
 
 enum MediaType {
 	VIDEO = 'VIDEO',
@@ -8,11 +8,18 @@ enum MediaType {
 	DATA = 'DATA'
 }
 
-let mediaPromise = (n: number) => new Promise((resolve, reject) => {
+interface Packet {
+	name: string
+	mediaType: MediaType
+	pts: number
+}
+
+let mediaPromise = (n: number, name: string): Promise<Packet> => new Promise((resolve, reject) => {
 	setTimeout(() => {
 		switch (n % 4) {
 			case 0:
 				resolve({
+					name,
 					mediaType: MediaType.VIDEO,
 					pts: (n / 4 | 0) * 90000
 				})
@@ -20,12 +27,14 @@ let mediaPromise = (n: number) => new Promise((resolve, reject) => {
 			case 1:
 			case 2:
 				resolve({
+					name,
 					mediaType: MediaType.AUDIO,
 					pts: (n / 4 | 0) * 90000
 				})
 				break
 			case 3:
 				resolve({
+					name,
 					mediaType: MediaType.DATA,
 					pts: (n / 4 | 0) * 90000
 				})
@@ -37,14 +46,41 @@ let mediaPromise = (n: number) => new Promise((resolve, reject) => {
 	}, n % 4 === 0 ? 5 : 1)
 })
 
-let source = H(async (push, next) => {
-	let packet = await mediaPromise(counter++)
+let mixerPromise = (s1: Packet, s2: Packet) => new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve({
+			name: `mix(${s1.name}, ${s2.name})`,
+			mediaType: s1.mediaType,
+			pts: s1.pts
+		})
+	}, 12)
+})
+
+let source1 : Highland.Stream<Packet> = H(async (push, next) => {
+	let packet = await mediaPromise(counter1++, 'Source1')
 	push(null, packet)
 	next()
 })
 
-let video = source.fork().filter((x: any) => x.mediaType === MediaType.VIDEO)
-let audio = source.fork().filter((x: any) => x.mediaType === MediaType.AUDIO)
+let source2 : Highland.Stream<Packet> = H(async (push, next) => {
+	let packet = await mediaPromise(counter2++, 'Source2')
+	push(null, packet)
+	next()
+})
 
-H([H([1, 2, 3]), H([4,5])]).sequence().each(H.log)
+let video1: Highland.Stream<Packet> = source1.fork().filter((x: any) => x.mediaType === MediaType.VIDEO)
+let audio1: Highland.Stream<Packet> = source1.fork().filter((x: any) => x.mediaType === MediaType.AUDIO)
+
+let video2: Highland.Stream<Packet> = source2.fork().filter((x: any) => x.mediaType === MediaType.VIDEO)
+let audio2: Highland.Stream<Packet> = source2.fork().filter((x: any) => x.mediaType === MediaType.AUDIO)
+
+// @ts-ignore: typescirpt deinition is wrong for zip
+let vmix = video1.zip(video2).flatMap((x: [Packet, Packet]) => H(mixerPromise(x[0], x[1])))
+
+// @ts-ignore: typescirpt deinition is wrong for zip
+let amix = audio1.zip(audio2).flatMap((x: [Packet, Packet]) => H(mixerPromise(x[0], x[1])))
+
+
+// @ts-ignore Typescript getting its knickers twisted over this: parameter for stream of streams
+H([vmix, amix]).merge().ratelimit(3, 200).each(H.log)
 //audio.each(H.log)
