ebe4ecb2b6ee271aaa18c2a5ec0722bbe6f26d8f
Richard Cartwright <spark@streampunk.media>
2019-09-12 20:56:21 +0100

    Creating EBU OS event demo.

--- scratch/ebu_oss_clunker.js
+++ scratch/ebu_oss_clunker.js
@@ -3,6 +3,7 @@ const macadam = require('macadam')
 const nodencl = require('nodencl');
 const rgbyuv = require('../process/rgbyuvPacker.js');
 const v210_io = require('../process/v210_io.js');
+const yuv422p10_io = require('../process/yuv422p10_io.js')
 const fs = require('fs').promises;
 const osc = require('osc')
 
@@ -33,22 +34,6 @@ __kernel void
   }
 `;
 
-// const testImage = `
-// __constant sampler_t smp =
-//       CLK_NORMALIZED_COORDS_FALSE
-//     | CLK_ADDRESS_CLAMP_TO_EDGE
-//     | CLK_FILTER_NEAREST;
-//
-// __kernel void mix(
-// __read_only image2d_t s1, __read_only image2d_t s2, float l, __write_only image2d_t d) {
-//  int2 p=(int2)(get_global_id(0),get_global_id(1));
-//  float4 i=read_imagef(s1,smp,p);
-//  float4 j=read_imagef(s2,smp,p);
-//  float r=1-l;
-//  write_imagef(d,p,(float4)(l*i.s0+r*j.s0,l*i.s1+r*j.s1,l*i.s2+r*j.s2,1.0f));
-// }
-// `;
-
 async function run() {
 	const platformIndex = 0;
 	const deviceIndex = 0;
@@ -65,10 +50,10 @@ async function run() {
 	const width = 1920;
 	const height = 1080;
 
-	const v210Loader1 = new rgbyuv.yuvLoader(context, colSpecRead, colSpecWrite, new v210_io.reader(width, height));
-  await v210Loader1.init();
-	const v210Loader2 = new rgbyuv.yuvLoader(context, colSpecRead, colSpecWrite, new v210_io.reader(width, height));
-  await v210Loader2.init();
+	const yuv422p10Loader1 = new rgbyuv.yuvLoader(context, colSpecRead, colSpecWrite, new yuv422p10_io.reader(width, height));
+  await yuv422p10Loader1.init();
+	const yuv422p10Loader2 = new rgbyuv.yuvLoader(context, colSpecRead, colSpecWrite, new yuv422p10_io.reader(width, height));
+  await yuv422p10Loader2.init();
 
   const v210Saver = new rgbyuv.yuvSaver(context, colSpecWrite, new v210_io.writer(width, height));
   await v210Saver.init();
@@ -78,9 +63,20 @@ async function run() {
 		globalWorkItems: Uint32Array.from([ width, height ])
 	});
 
+	const lumaBytes = yuv422p10_io.getPitchBytes(width) * height;
+  const chromaBytes = lumaBytes / 2;
 	const numBytesV210 = v210_io.getPitchBytes(width) * height;
-	const v210Src1 = await context.createBuffer(numBytesV210, 'readonly', 'coarse');
-	const v210Src2 = await context.createBuffer(numBytesV210, 'readonly', 'coarse');
+
+	const srcs1 = [
+    await context.createBuffer(lumaBytes, 'readonly', 'coarse'),
+    await context.createBuffer(chromaBytes, 'readonly', 'coarse'),
+    await context.createBuffer(chromaBytes, 'readonly', 'coarse')
+  ];
+	const srcs2 = [
+		await context.createBuffer(lumaBytes, 'readonly', 'coarse'),
+		await context.createBuffer(chromaBytes, 'readonly', 'coarse'),
+		await context.createBuffer(chromaBytes, 'readonly', 'coarse')
+	];
 
 	const numBytesRGBA = width * height * 4 * 4;
 	const rgbaDst1 = await context.createBuffer(numBytesRGBA, 'readwrite', 'coarse');
@@ -97,10 +93,16 @@ async function run() {
 
 	async function processFrame(b1, b2, fl) {
 		let res = []
-		await v210Src1.hostAccess('writeonly', b1)
-		await v210Src2.hostAccess('writeonly', b2)
-		res[0] = await v210Loader1.fromYUV({ source: v210Src1, dest: rgbaDst1 })
-		res[1] = await v210Loader2.fromYUV({ source: v210Src2, dest: rgbaDst2 })
+		await Promise.all([
+			srcs1[0].hostAccess('writeonly', b1[0].slice(0, lumaBytes)),
+	  	srcs1[1].hostAccess('writeonly', b1[1].slice(0, chromaBytes)),
+	  	srcs1[2].hostAccess('writeonly', b1[2].slice(0, chromaBytes)),
+			srcs2[0].hostAccess('writeonly', b2[0].slice(0, lumaBytes)),
+			srcs2[1].hostAccess('writeonly', b2[1].slice(0, chromaBytes)),
+			srcs2[2].hostAccess('writeonly', b2[2].slice(0, chromaBytes)) ]);
+		[ res[0], res[1] ] = await Promise.all([
+			yuv422p10Loader1.fromYUV({ sources: srcs1, dest: rgbaDst1 }),
+			yuv422p10Loader2.fromYUV({ sources: srcs2, dest: rgbaDst2 }) ]);
 		//await rgbaDst.hostAccess('readonly')
 		// dumpFloatBuf(rgbaDst, 1920, 2, 4);
 		res[2] = await testImageProgram.run({input1: rgbaDst1, input2: rgbaDst2, fl: fl, output: imageDst})
@@ -121,15 +123,24 @@ async function run() {
 
 	let start = process.hrtime();
 
-	let frameList1 = await fs.readdir('../media/EBU_test_sets/filexchange.ebu.ch/EBU test sets - Creative Commons (BY-NC-ND)/HDTV test sequences/1080i25/Graphics_1080i_/Graphics_1080i_')
-	frameList1 = frameList1.filter(f => f.endsWith('v210'))
-	let frameList2 = await fs.readdir('../media/EBU_test_sets/filexchange.ebu.ch/EBU test sets - Creative Commons (BY-NC-ND)/HDTV test sequences/1080i25/girlflower1_1080i_/girlflower1_1080i_')
-	frameList2 = frameList2.filter(f => f.endsWith('v210'))
-	console.log(frameList2)
+	let dm1 = await beamy.demuxer('file:../media/dpp/AS11_DPP_HD_EXAMPLE_1.mxf')
+	await dm1.seek({ time: 240.0 })
+	let dec1 = await beamy.decoder({ demuxer: dm1, stream_index: 0 })
+	let dm2 = await beamy.demuxer('file:../media/dpp/AS11_DPP_HD_EXAMPLE_1.mxf')
+	await dm2.seek({ time: 360.0 })
+	let dec2 = await beamy.decoder({ demuxer: dm1, stream_index: 0 })
 
-	let counter = 0
-	let data1 = Buffer.alloc(5529600)
-	let data2 = Buffer.alloc(5529600)
+	async function read1() {
+		let p = await dm1.read()
+		if (p.stream_index === 0) return p
+		return read1()
+	}
+
+	async function read2() {
+		let p = await dm2.read()
+		if (p.stream_index === 0) return p
+		return read2()
+	}
 
 	let fl = 0.5
 
@@ -144,22 +155,30 @@ async function run() {
 			// console.log(fl)
 		}
 	});
-	oscUdp.open()
+	oscUdp.open();
+
+	let result = []
+	let counter = 0
 
 	while (true) {
-		let fh1 = await fs.open('../media/EBU_test_sets/filexchange.ebu.ch/EBU test sets - Creative Commons (BY-NC-ND)/HDTV test sequences/1080i25/Graphics_1080i_/Graphics_1080i_/' + frameList1[counter % frameList1.length], 'r')
-		let fh2 = await fs.open('../media/EBU_test_sets/filexchange.ebu.ch/EBU test sets - Creative Commons (BY-NC-ND)/HDTV test sequences/1080i25/girlflower1_1080i_/girlflower1_1080i_/' + frameList2[counter % frameList2.length], 'r')
-		await Promise.all([fh1.read(data1, 0, data1.length, 0), fh2.read(data2, 0, data2.length, 0)])
-		await fh1.close()
-		await fh2.close()
-		let stamp = process.hrtime()
-		await processFrame(data1, data2, fl)
-		let total = process.hrtime(stamp)
-		await playback.displayFrame(v210Dst)
+		let work = []
+		let stamp = process.hrtime();
+		work[0] = Promise.all([read1(), read2()]) // add to clunker
+		if (result.length >= 1) {
+			work[1] = Promise.all([dec1.decode(result[0][0]), dec2.decode(result[0][1])])
+		}
+		if (result.length >= 2) {
+			work[2] = processFrame(result[1][0].frames[0].data, result[1][1].frames[0].data, fl)
+		}
+		if (result.length >= 3) {
+			work[3] = playback.displayFrame(v210Dst)
+		}
+		// console.log(work)
+		result = await Promise.all(work)
 		let diff = process.hrtime(start)
 		let wait = (counter * 40) - ((diff[0] * 1000) + (diff[1] / 1000000 | 0) )
 		await waitForIt(wait)
-		console.log(`Clunk ${counter++} completed in ${total} waiting ${wait}`)
+		console.log(`Clunk ${counter++} completed in ${process.hrtime(stamp)} waiting ${wait}`)
 	}
 }
 
