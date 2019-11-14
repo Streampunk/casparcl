ff3dbe4aeb378ca0b2fafc77b4aabc7eb2dd2c9c
Simon Rogers <scriptorian@streampunk.media>
2019-11-14 17:22:21 +0000

    Update to use new io and switcher components

--- scratch/elecular_clunker2.js
+++ scratch/elecular_clunker2.js
@@ -17,15 +17,8 @@ const request = require('request-promise-native')
 const Koa = require('koa');
 const cors = require('@koa/cors');
 const beamcoder = require('beamcoder');
-const rgbrgb = require('../process/rgbrgbPacker.js');
-const rgbyuv = require('../process/rgbyuvPacker.js');
-const rgba8_io = require('../process/rgba8_io.js');
-const bgra8_io = require('../process/bgra8_io.js');
-const v210_io = require('../process/v210_io.js');
-const yuv422p10_io = require('../process/yuv422p10_io.js');
-const imageProcess = require('../process/imageProcess.js');
-const combine = require('../process/combine.js');
-const resize = require('../process/resize.js');
+const io = require('../process/io.js');
+const vidSwitch = require('../process/switcher.js');
 const macadam = require('macadam')
 
 const kapp = new Koa();
@@ -33,65 +26,38 @@ kapp.use(cors());
 
 const width = 1920;
 const height = 1080;
-const pixelsPerWorkItem = 128;
 
 let bgra8Loader;
+let vidSwitcher;
 let webSaver;
 let v210Saver;
-let srcs;
-let bgra8Src;
 let rgbaBG;
 let rgbaOV;
 let rgbaDst;
-let rgbaDstWeb;
 let v210Dst;
 let webDst;
 let lastWeb;
-let combiner;
-let resizer;
-let lumaBytes;
-let chromaBytes;
 
 async function processFrame(bg, overlay) {
-  // let start = process.hrtime();
-	await Promise.all([
-		srcs[0].hostAccess('writeonly', bg[0].slice(0, lumaBytes)),
-		srcs[1].hostAccess('writeonly', bg[1].slice(0, chromaBytes)),
-		srcs[2].hostAccess('writeonly', bg[2].slice(0, chromaBytes))
-	])
+  let start = process.hrtime();
 
-  await bgra8Src.hostAccess('writeonly', overlay);
+  await yuv422p10Loader.processFrame(bg, rgbaBG);
+  await bgra8Loader.processFrame(overlay, rgbaOV);
 
-  let timings;
-  timings = await yuv422p10Loader.fromYUV({ sources: srcs, dest: rgbaBG });
-  // console.log(`${timings.dataToKernel}, ${timings.kernelExec}, ${timings.dataFromKernel}, ${timings.totalTime}`);
+  timings = await vidSwitcher.processFrame(
+    [{ input: rgbaBG, scale: 0.75, offsetX: 0.0, offsetY: 0.0, flipH: true, flipV: false },
+     { input: rgbaBG, scale: 0.75, offsetX: 0.0, offsetY: 0.0, flipH: false, flipV: true }],
+    { wipe: true, frac: 0.5 },
+    rgbaOV,
+    rgbaDst
+  )
 
-  timings = await bgra8Loader.fromRGB({ source: bgra8Src, dest: rgbaOV });
-  // console.log(`${timings.dataToKernel}, ${timings.kernelExec}, ${timings.dataFromKernel}, ${timings.totalTime}`);
-
-  timings = await combiner.run({ bgIn: rgbaBG, ovIn: rgbaOV, output: rgbaDst });
-  // console.log(`${timings.dataToKernel}, ${timings.kernelExec}, ${timings.dataFromKernel}, ${timings.totalTime}`);
-
-  timings = await resizer.run({ input: rgbaDst, output: rgbaDstWeb });
-  // console.log(`${timings.dataToKernel}, ${timings.kernelExec}, ${timings.dataFromKernel}, ${timings.totalTime}`);
-
-  timings = await v210Saver.toYUV({ source: rgbaDst, dest: v210Dst });
-  // console.log(`${timings.dataToKernel}, ${timings.kernelExec}, ${timings.dataFromKernel}, ${timings.totalTime}`);
-
-  await v210Dst.hostAccess('readonly');
-
-  timings = await webSaver.toRGB({ source: rgbaDstWeb, dest: webDst });
-  // console.log(`${timings.dataToKernel}, ${timings.kernelExec}, ${timings.dataFromKernel}, ${timings.totalTime}`);
-
-  await webDst.hostAccess('readonly');
+  await v210Saver.processFrame(rgbaDst, v210Dst);
+  await webSaver.processFrame(rgbaDst, webDst);
 
   // let end = process.hrtime(start);
   // console.log('OpenCL:', end);
 }
-
-let timer = (w) => new Promise((resolve) => {
-	setTimeout(resolve, w);
-})
 
 async function init () {
   const platformIndex = 0;
@@ -106,58 +72,37 @@ async function init () {
   let demuxer = await beamcoder.demuxer('../../media/dpp/AS11_DPP_HD_EXAMPLE_1.mxf');
   await demuxer.seek({ time: 40 });
   let decoder = beamcoder.decoder({ name: 'h264' });
-  let encParams = {
-    name: 'v210',
-    width: width,
-    height: height,
-    time_base: [1, 25],
-    framerate: [25, 1],
-    pix_fmt: 'yuv422p10le',
-  };
-  let encoder = beamcoder.encoder(encParams);
 
 	const bgColSpecRead = '709';
   const ovColSpecRead = 'sRGB';
   const colSpecWrite = '709';
   const webColSpecWrite = 'sRGB';
 
-  yuv422p10Loader = new rgbyuv.yuvLoader(context, bgColSpecRead, colSpecWrite,  new yuv422p10_io.reader(width, height));
-  await yuv422p10Loader.init();
-  v210Saver = new rgbyuv.yuvSaver(context, colSpecWrite, new v210_io.writer(width, height));
-  await v210Saver.init();
+  yuv422p10Loader = new io.toRGBA(context, width, height, 'yuv422p10');
+  await yuv422p10Loader.init({colSpecRead: bgColSpecRead, colSpecWrite: colSpecWrite});
 
-  bgra8Loader = new rgbrgb.rgbLoader(context, ovColSpecRead, colSpecWrite, new bgra8_io.reader(width, height));
-  await bgra8Loader.init();
-  webSaver = new rgbrgb.rgbSaver(context, webColSpecWrite, new rgba8_io.writer(width / 2, height / 2));
-  await webSaver.init();
+  bgra8Loader = new io.toRGBA(context, width, height, 'bgra8');
+  await bgra8Loader.init({colSpecRead: ovColSpecRead, colSpecWrite: colSpecWrite});
 
-  combiner = new imageProcess(context, width, height, new combine({ numInputs: 2 }));
-  await combiner.init();
-  resizer = new imageProcess(context, width / 2, height / 2, new resize({}));
-  await resizer.init();
+  vidSwitcher = new vidSwitch(context, width, height, 2, 1);
+  await vidSwitcher.init();
 
-	lumaBytes = yuv422p10_io.getPitchBytes(width) * height;
-	chromaBytes = lumaBytes / 2;
-	const numBytesV210 = v210_io.getPitchBytes(width) * height;
+  v210Saver = new io.fromRGBA(context, width, height, 'v210');
+  await v210Saver.init({ colSpec: colSpecWrite });
 
-	srcs = [
-		await context.createBuffer(lumaBytes, 'readonly', 'coarse'),
-		await context.createBuffer(chromaBytes, 'readonly', 'coarse'),
-		await context.createBuffer(chromaBytes, 'readonly', 'coarse')
-	];
-  const numBytesBGRA8 = bgra8_io.getPitchBytes(width) * height;
-  bgra8Src = await context.createBuffer(numBytesBGRA8, 'readonly', 'coarse');
+  webSaver = new io.fromRGBA(context, width / 2, height / 2, 'rgba8');
+  await webSaver.init({ colSpec: webColSpecWrite, srcWidth: width, srcHeight: height });
 
   const numBytesRGBA = width * height * 4 * 4;
   rgbaBG = await context.createBuffer(numBytesRGBA, 'readwrite', 'coarse', { width: width, height: height });
   rgbaOV = await context.createBuffer(numBytesRGBA, 'readwrite', 'coarse', { width: width, height: height });
+
   rgbaDst = await context.createBuffer(numBytesRGBA, 'readwrite', 'coarse', { width: width, height: height });
-  rgbaDstWeb = await context.createBuffer(width * height * 4, 'readwrite', 'coarse', { width: width / 2, height: height / 2 });
 
-  v210Dst = await context.createBuffer(numBytesV210, 'writeonly', 'coarse');
-  webDst = await context.createBuffer(numBytesBGRA8 / 4, 'writeonly', 'coarse');
+  v210Dst = await context.createBuffer(v210Saver.getNumBytes(), 'writeonly', 'coarse');
+  webDst = await context.createBuffer(webSaver.getNumBytes(), 'writeonly', 'coarse');
 
-  lastWeb = Buffer.alloc(numBytesBGRA8 / 4);
+  lastWeb = Buffer.alloc(webSaver.getNumBytes());
 
 	kapp.use(async ctx => {
 		ctx.body = lastWeb;
